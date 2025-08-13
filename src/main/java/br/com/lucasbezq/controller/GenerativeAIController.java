7efca98c59746bae730c4438869ab2433c6600d6
Lucas Ezequiel <lucasbarrosez@gmail.com>
2025-08-12 22:53:44 -0300

    feat: add ImageService and endpoint for image generation

--- src/main/java/br/com/lucasbezq/controller/GenerativeAIController.java
+++ src/main/java/br/com/lucasbezq/controller/GenerativeAIController.java
@@ -1,20 +1,25 @@
 package br.com.lucasbezq.controller;
 
 import br.com.lucasbezq.service.ChatService;
+import br.com.lucasbezq.service.ImageService;
 import br.com.lucasbezq.service.RecipeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GenerativeAIController {
 
     private final ChatService chatService;
     private final RecipeService recipeService;
+    private final ImageService imageService;
 
-    public GenerativeAIController(ChatService chatService, RecipeService recipeService) {
+    public GenerativeAIController(ChatService chatService, RecipeService recipeService, ImageService imageService) {
         this.chatService = chatService;
         this.recipeService = recipeService;
+        this.imageService = imageService;
     }
 
     @GetMapping("/ask-ai")
@@ -32,5 +37,18 @@ public class GenerativeAIController {
                                  @RequestParam(defaultValue = "any") String cuisine,
                                  @RequestParam(defaultValue = "none") String dietaryRestrictions) {
         return recipeService.generateRecipe(ingredients, cuisine, dietaryRestrictions);
+    }
+
+    @GetMapping("/images")
+    public List<String> generateImage(@RequestParam String prompt,
+                                      @RequestParam(defaultValue = "hd") String quality,
+                                      @RequestParam(defaultValue = "1") Integer quantity,
+                                      @RequestParam(defaultValue = "1024") Integer height,
+                                      @RequestParam(defaultValue = "1024") Integer width) {
+        var responses = imageService.generateImage(prompt, quality, quantity, height, width);
+        return responses.getResults()
+                .stream()
+                .map(result -> result.getOutput().getUrl())
+                .toList();
     }
 }
